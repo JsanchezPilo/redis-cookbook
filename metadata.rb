8a3ff9c326524bbaf47a8fe3e8fcbced5e0a4fd5
JsanchezPilo <38760945+JsanchezPilo@users.noreply.github.com>
2018-09-22 21:58:50 -0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license 'All rights reserved'
 description 'Installs/Configures redis-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.1'
-#depends	'apt'
+depends	'apt'
