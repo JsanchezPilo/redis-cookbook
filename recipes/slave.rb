106a2503a6b385f77ecfe6f28c9c48e4d90ec167
JsanchezPilo <38760945+JsanchezPilo@users.noreply.github.com>
2018-09-23 21:57:58 -0600

    Update slave.rb

--- recipes/slave.rb
+++ recipes/slave.rb
@@ -15,6 +15,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+include_recipe 'systemd'
 
 master = search(:node, "tags:cluster").first
 Chef::Log.info("**********The public IP address is: '#{master[:private_ip]}'**********")
