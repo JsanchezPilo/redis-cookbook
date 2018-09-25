a9296f83be24cd5b7916ecf13a02d2bf88b931d9
JsanchezPilo <38760945+JsanchezPilo@users.noreply.github.com>
2018-09-24 20:00:38 -0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'redis-cookbook'
 maintainer 'JsanchezPilo'
 maintainer_email 'jlsgsistemas@gmail.com'
-license 'All rights reserved'
+license 'MIT'
 description 'Installs/Configures redis-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.1'
